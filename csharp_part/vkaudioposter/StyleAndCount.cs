7c1ebc4ccc74dbc68ea8e9c36389eebb50f9df14
rhiskey_whiskey <first84@yandex.ru>
2018-11-16 02:19:10 +0300

    Added full project files
    Deleted unneccessary debug files

---
+++ csharp_part/vkaudioposter/StyleAndCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vkaudioposter
+{
+    class StyleAndCount
+    {
+        public string StyleName;
+        public int Count;
+    }
+}
